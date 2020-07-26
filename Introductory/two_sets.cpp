146ebe8896f360ac5eeb3139e1b55deceb51471e
Kevin Zhao <kzhao16@gmail.com>
2020-07-25 22:57:35 -0400

    finished the two set problem

--- Introductory/two_sets.cpp
+++ Introductory/two_sets.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <string>
-
-using namespace std;
+#include <unordered_set>
 
 #define ll long long
 
-int main() {
-	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-	
-	int n;
-	cin >> n;
+using namespace std;
 
-	int sum = n*(n+1)/2;
-	if ( % 2) {
-		cout << "NO";
-	} else {
-		cout << "YES" << endl;
-		int sum = 
-	}
+int main() {
+    ll n;
+    cin >> n;
+
+    if (n % 4 > 0 && (n+1) % 4 > 0) {
+        cout << "NO" << endl;
+    } else {
+        cout << "YES" << endl;
+        ll half = n*(n+1)/4;
+        ll top = (n % 4 == 0) + n;
+        ll start = n;
+        unordered_set<int> first;
+
+        while (half > 0) {
+            if (start > 0) first.insert(start);
+            if (top - start > 0) first.insert(top - start);
+            half -= top;
+            start--;
+        }    
+
+        cout << first.size() << endl;
+        for (int i=1; i<=n; i++) {
+            if (first.count(i)) cout << i << " ";
+        }
+        cout << endl;
+
+        cout << n - first.size() << endl;
+        for (int i=1; i<=n; i++) {
+            if (!first.count(i)) cout << i << " ";
+        }
+    }
 }
+
